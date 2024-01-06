f4a6c284ecaa9e239a6e543c2e62f8e0fab78dd2
nuko <nuko@shimeji.cafe>
2024-01-06 21:44:17 +1300

    factory disko init

--- disko/factory.nix
+++ disko/factory.nix
@@ -5,3 +5,100 @@
     sata1 = "ata-Samsung_SSD_860_EVO_1TB_S3YBNB0N912941N";
     sata2 = "ata-Samsung_SSD_870_EVO_1TB_S6P5NX0T317019K";
   in {
+    disk = {
+      # root / system drive.
+      nvme1 = {
+        type = "disk";
+        device = "${nvme1}";
+        content = {
+          type = "gpt";
+          partition = {
+            boot = {
+              device = "${nvme1}-part1";
+              type = "EF00";
+              size = "1G";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            swap = {
+              device = "${nvme1}-part2";
+              size = "64gb";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+              };
+            };
+            root = {
+              device = "${nvme1}-part3";
+              size = "100%";
+              conent = {
+                type = "zfs";
+                pool = "rpool";
+              };
+            };
+          };
+        };
+      };
+      sata1 = {
+        type = "disk";
+        device = "${sata1}";
+        content = {
+          type = "gpt";
+          partitions.zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "spool";
+            };
+          };
+        };
+      };
+      sata2 = {
+        type = "disk";
+        device = "${sata2}";
+        content = {
+          type = "gpt";
+          partitions.zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "spool";
+            };
+          };
+        };
+      };
+    };
+    zpool = {
+      rpool = {
+        type = "zpool";
+        rootFsOptions = {
+          compression = "zstd";
+          xattr = "sa";
+          mountpoint = "none";
+        };
+        datasets."root" = {
+          type = "zfs_fs";
+          options.mountpoint = "legacy";
+          mountpoint = "/";
+        };
+      };
+      spool = {
+        type = "zpool";
+        mode = "raid0";
+        rootFsOptions = {
+          compression = "zstd";
+          xattr = "sa";
+          mountpoint = "none";
+        };
+        datasets."root" = {
+          type = "zfs_fs";
+          options.mountpoint = "legacy";
+          mountpoint = "/storage";
+        };
+      };
+    };
+  };
+}
