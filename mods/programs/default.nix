e82fb085e33fed8563a20f5284599e2f6325d8d2
nuko <nuko@shimeji.cafe>
2024-01-21 18:28:33 +1300

    more mod moment

--- mods/programs/default.nix
+++ mods/programs/default.nix
@@ -3,13 +3,43 @@
   lib,
   pkgs,
   ...
-}: {
-  options.program.prism = lib.mkEnableOption "";
-  config = lib.mkIf config.program.prism {
-    users.users.main.packages = [pkgs.prismlauncher-qt5];
-    environment.etc = {
+}: let
+  inherit (lib) mkEnableOption mkIf;
+in {
+  options.program = {
+    prism = mkEnableOption "";
+    git = mkEnableOption "";
+    steam = mkEnableOption "";
+  };
+  config = let
+    inherit (config.program) prism git steam;
+  in {
+    ### prism
+    users.users.main.packages = mkIf prism [pkgs.prismlauncher-qt5];
+    environment.etc = mkIf prism {
       "jdks/17".source = pkgs.openjdk17 + /bin;
       "jdks/8".source = pkgs.openjdk8 + /bin;
+    };
+    ### git
+    programs.git = mkIf git {
+      enable = true;
+      config = {
+        init = {
+          defaultBranch = "main";
+        };
+        user = {
+          name = "nuko";
+          email = "host@nuko.city";
+        };
+      };
+    };
+    ### steam
+    programs.steam = mkIf steam {
+      enable = true;
+      package = pkgs.steam.override {
+        # required for source1 games.
+        extraLibraries = pkgs: [pkgs.wqy_zenhei pkgs.pkgsi686Linux.gperftools];
+      };
     };
   };
 }
--- mods/services/default.nix
+++ mods/services/default.nix
@@ -6,10 +6,11 @@
   options.service = {
     fail2ban = lib.mkEnableOption "";
     postgres = lib.mkEnableOption "";
+    openssh = lib.mkEnableOption "";
   };
   config = let
     inherit (lib) mkIf;
-    inherit (config.service) fail2ban postgres;
+    inherit (config.service) fail2ban postgres openssh;
   in {
     services = {
       fail2ban = mkIf fail2ban {
@@ -22,6 +23,14 @@
       postgresql = mkIf postgres {
         enable = true;
         dataDir = "/storage/volumes/postgres";
+      };
+      openssh = mkIf openssh {
+        enable = true;
+        openFirewall = true;
+        settings = {
+          PermitRootLogin = "no";
+          PasswordAuthentication = false;
+        };
       };
     };
   };
