c7a9ef039ee92cbdcf10b239e592d36a6697ce70
nuko <nuko@shimeji.cafe>
2024-02-06 10:50:19 +1300

    nixpgks path

--- mods/misc/nix.nix
+++ mods/misc/nix.nix
@@ -23,6 +23,7 @@
         auto-optimise-store = true;
         allowed-users = [ "@wheel" ];
         use-xdg-base-directories = true;
+        nix-path = ["nixpkgs=flake:nixpkgs"];
       };
       optimise.automatic = true;
       gc = {
