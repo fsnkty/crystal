dae3b9dafde29e3fc3e469ec2e9e19a79a9c2c72
fsnkty <fsnkty@pm.me>
2025-02-21 14:42:55 +1300

    factory: git config

--- hosts/factory.nix
+++ hosts/factory.nix
@@ -23,10 +23,25 @@
   };
 
   # wsl vscode server
-  environment.systemPackages = [ pkgs.wget pkgs.nixpkgs-fmt];
+  environment.systemPackages = [ pkgs.wget pkgs.nixpkgs-fmt ];
   programs.nix-ld = {
     enable = true;
     package = pkgs.nix-ld-rs;
+  };
+  # github
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+      user = {
+        name = "fsnkty";
+        email = "fsnkty@pm.me";
+        signingkey = "/home/fsnkty/.ssh/id_ed25519.pub";
+      };
+      gpg.format = "ssh";
+      commit.gpgsign = true;
+    };
   };
 
   time.timeZone = "NZ";
