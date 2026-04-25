66a40753f3598ceaa7990cbdcf38d10e75234232
fsnkty <fsnkty@pm.me>
2026-04-25 20:20:29 +1200

    portal

--- hosts/portal.nix
+++ hosts/portal.nix
@@ -14,7 +14,7 @@
     description = "Madison";
     extraGroups = [ "networkmanager" "wheel" ];
   };
-
+  programs.hyprland.enable = true;
   services = {
     pulseaudio.enable = false;
     pipewire = {
@@ -33,22 +33,71 @@
     networkmanager.enable = true;
   };
 
+  # finger print setup
+  systemd.services.fprintd = {
+    wantedBy = ["multi-user.target"];
+    serviceConfig.Type = "simple";
+  };
+  nixpkgs.overlays = [
+    (final: prev: {
+      # Use gugah's libfprint fork with vfs009x support
+      libfprint = prev.libfprint.overrideAttrs (old: rec {
+        pname = "libfprint-vfs009x";
+
+        # This is just cosmetic; set it to something that helps you remember
+        version = "1.94.9+vfs009x";
+
+        # Use the values from:
+        #   nix-prefetch-git --url https://gitlab.archlinux.org/gugah/libfprint.git \
+        #                    --rev refs/heads/vfs009x
+        src = final.fetchgit {
+          url    = "https://gitlab.archlinux.org/gugah/libfprint.git";
+          rev    = "450e6aea0f5c92b3719d910c0defb2c85b0746df";
+          sha256 = "sha256-Rm62zo2PRO1GlN8I9+r7MOl9q4AlUixrD1Y13Of8Xmw=";
+        };
+    buildInputs = (old.buildInputs or [ ]) ++ [ final.nss ];
+
+        # Keep any patches Nixpkgs already applies to libfprint
+        patches = (old.patches or [ ]);
+      });
+
+      # Optionally expose it explicitly as a separate name too
+      libfprint-vfs009x = final.libfprint;
+    })
+  ];
+  services.fprintd = {
+    enable = true;
+    tod.enable = false;
+  };
+  # rest of hardware
   boot = {
+    plymouth = {
+      enable = true;
+    };
+    zswap.enable = true;
     loader = {
+      timeout = 0;
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
     kernelPackages = pkgs.linuxPackages_latest;
+    kernelModules = [ "kvm-intel" ];
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
     initrd = {
       availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+      systemd.enable = true;
+      verbose = false;
     };
-    kernelModules = [ "kvm-intel" ];
+    consoleLogLevel = 3;
   };
   hardware = {
     cpu.intel.updateMicrocode = true;
     enableRedistributableFirmware = true;
   };
-
   fileSystems = {
     "/boot" = {
       device = "/dev/disk/by-uuid/2906-7F1F";
