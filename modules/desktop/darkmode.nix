a69c985a9d8196e32ba4b0a0758ac8c847437bdf
fsnkty <fsnkty@pm.me>
2026-06-02 11:53:06 +1200

    force fonts declaration to avoid nixpkgs kde module font sets

--- modules/desktop/darkmode.nix
+++ modules/desktop/darkmode.nix
@@ -6,7 +6,8 @@
 {
   options.crystal.desktop.darkmode.enable = lib.mkEnableOption "";
   config = lib.mkIf config.crystal.desktop.darkmode.enable {
-    # gtk "dark mode" pref, ( some non-gtk guis will also listen to this )
+    # gtk "dark mode" pref
+    # some non-gtk guis will also listen to this, e.g.. chromium & vscodium's "system" opt
     programs.dconf.profiles.user.databases = [
       {
         settings."org/gnome/desktop/interface" = {
--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -10,9 +10,19 @@
   config = lib.mkIf config.crystal.desktop.fonts.setup {
     # better for hidpi
     console.font = "${pkgs.terminus_font}/share/consolefonts/ter-116n.psf.gz";
-    fonts.packages = [
-      inputs.apple-fonts.packages.x86_64-linux.sf-pro-nerd
-      inputs.apple-fonts.packages.x86_64-linux.sf-mono-nerd
-    ];
+    fonts = {
+      packages = lib.mkForce [
+        inputs.apple-fonts.packages.x86_64-linux.sf-pro-nerd
+        inputs.apple-fonts.packages.x86_64-linux.sf-mono-nerd
+      ];
+      enableDefaultPackages = false;
+      fontconfig = {
+        defaultFonts = lib.mkForce {
+          monospace = [ "SFMono Nerd Font" ];
+          sansSerif = [ "SFPro Nerd Font" ];
+          serif = [ "SFPro Nerd Font" ];
+        };
+      };
+    };
   };
 }
